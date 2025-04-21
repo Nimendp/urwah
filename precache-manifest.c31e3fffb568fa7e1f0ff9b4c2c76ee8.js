b0d51592c5f079751d01eb14b4e50260b10f25d0
urwah <yunusurwah91@gmail.com>
2025-04-21 22:29:59 +0800

    Updates

---
+++ precache-manifest.c31e3fffb568fa7e1f0ff9b4c2c76ee8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "974ba91ddc9f8201a17d2557cd9ff1e5",
+    "url": "/urwah/index.html"
+  },
+  {
+    "revision": "7643596d24f986fc2a7f",
+    "url": "/urwah/static/css/main.b9f68170.chunk.css"
+  },
+  {
+    "revision": "b82752955c59224008c5",
+    "url": "/urwah/static/js/2.b556ecb6.chunk.js"
+  },
+  {
+    "revision": "4c3dea1aafd2000e2636344fbce476ea",
+    "url": "/urwah/static/js/2.b556ecb6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "be09ec47b427b4707b52",
+    "url": "/urwah/static/js/3.3bc29157.chunk.js"
+  },
+  {
+    "revision": "7643596d24f986fc2a7f",
+    "url": "/urwah/static/js/main.d6faeba9.chunk.js"
+  },
+  {
+    "revision": "8182e89db313f08c0a80",
+    "url": "/urwah/static/js/runtime-main.4a39bc66.js"
+  }
+]);
